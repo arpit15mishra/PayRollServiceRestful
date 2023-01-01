f8834aee2951cfd4f80ec5dd6d8f8103a3bd8d8e
arpitmishra <arpitmishra@localhost>
2023-01-01 22:23:11 +0530

    added all employee

--- src/main/java/com/example/PayRollServiceRestful/EmployeeController.java
+++ src/main/java/com/example/PayRollServiceRestful/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,24 @@ public class EmployeeController {
 	public Optional<Employee> getEmployeeById(@PathVariable Long id) {
 		return empRepository.findById(id);
 		
+	}
+	
+	@PutMapping("/employees/{id}")
+	public Employee modifyEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+		return empRepository.findById(id).
+				map(employee->{
+					employee.setName(newEmployee.getName());
+					employee.setRole(newEmployee.getRole());
+					employee.setAge(newEmployee.getAge());
+					employee.setSalary(newEmployee.getSalary());
+					return empRepository.save(employee);
+					
+					
+				}).
+				orElseGet(() -> {
+			        newEmployee.setId(id);
+			        return empRepository.save(newEmployee);
+			      });
 	}
 
 }
