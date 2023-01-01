d1fc019b99f99bc393f2a57e8687fb8158bf5e72
arpitmishra <arpitmishra@localhost>
2023-01-01 15:02:38 +0530

    first commit

---
+++ src/main/java/com/example/PayRollServiceRestful/PayRollServiceRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.PayRollServiceRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayRollServiceRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayRollServiceRestfulApplication.class, args);
+	}
+
+}
