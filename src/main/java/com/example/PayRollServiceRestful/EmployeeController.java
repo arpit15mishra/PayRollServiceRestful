b994b58c600d5d3bd08b1fdd3f7d075b248b1617
arpitmishra <arpitmishra@localhost>
2023-01-01 18:09:58 +0530

    added get and post call

--- src/main/java/com/example/PayRollServiceRestful/EmployeeController.java
+++ src/main/java/com/example/PayRollServiceRestful/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.example.PayRollServiceRestful;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class EmployeeController {
@@ -17,6 +22,17 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAll() {
 		return empRepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee emp) {
+		return empRepository.save(emp);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+		return empRepository.findById(id);
+		
 	}
 
 }
